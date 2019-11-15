8763e64c299f2c76a7b7b46af686abf28deb46dc
quinnc <qcollins6@cps.edu>
2019-11-14 21:28:48 -0600

    Fully completed DFT function, now returns a matrix

--- dft.py
+++ dft.py
@@ -3,15 +3,22 @@ import numpy as np
 # Input is a complex number array
 testNum = np.array([1, 2, 3, 4, 5])
 
-def dft(amps):
-    output = np.array([0j, 0j, 0j, 0j, 0j])
-    n_amps = len(amps)
-    for k in range(n_amps):
-        for n in range(n_amps):
-            #Implement the sum formula
-            output[k] += amps[n]*np.e**(-2j*(np.pi)*(n)*(k)/n_amps)
-        
-    return output
+def dft(signal, twiddleMat=None):
+    
+    l_signal = len(signal)
+    
+    if twiddleMat is None:
+        #Generate empty complex # array
+        twiddleMat = np.full((l_signal, l_signal), 0j)
+        #summation
+        for k in range(l_signal):
+            for n in range(l_signal):
+                #Implement the sum formula
+                twiddleMat[k][n] += np.e**(-2j*(np.pi)*(n)*(k)/l_signal)
+    #reutrn the matrix multiplcation
+    return np.matmul(twiddleMat, signal)
+
+    
 
     
     
