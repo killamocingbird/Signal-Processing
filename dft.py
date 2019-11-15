3a8de5adb323a1433b3709824acab18a51eed47b
quinnc <qcollins6@cps.edu>
2019-11-14 20:41:39 -0600

    Implemented DFT formula with numpy to convert amplitudes to complex nums

--- dft.py
+++ dft.py
@@ -1,3 +1,17 @@
+import numpy as np
 
-def dft(x):
+# Input is a complex number array
+testNum = np.array([1, 2, 3, 4, 5])
+
+def dft(amps):
+    output = np.array([0j, 0j, 0j, 0j, 0j])
+    n_amps = len(amps)
+    for k in range(n_amps):
+        for n in range(n_amps):
+            output[n] += amps[n]*np.e**(-2j*(np.pi)*(n)*(k)/n_amps)
+        
+    return output
+    
+    
+    
     
