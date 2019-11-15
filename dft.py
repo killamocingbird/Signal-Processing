03f707e0d88c033aee5d7b985a7ddb4a02e2412e
Justin Wang <jlwang5@illinois.edu>
2019-11-14 21:39:10 -0600

    Fix naming convention

--- dft.py
+++ dft.py
@@ -1,25 +1,15 @@
 import numpy as np
 
-# Input is a complex number array
-testNum = np.array([1, 2, 3, 4, 5])
-
-def dft(signal, twiddleMat=None):
+def dft(signal, twiddle_mat=None):
     
     l_signal = len(signal)
     
-    if twiddleMat is None:
-        #Generate empty complex # array
-        twiddleMat = np.full((l_signal, l_signal), 0j)
-        #summation
-        for k in range(l_signal):
-            for n in range(l_signal):
-                #Implement the sum formula
-                twiddleMat[k][n] += np.e**(-2j*(np.pi)*(n)*(k)/l_signal)
-    #reutrn the matrix multiplcation
-    return np.matmul(twiddleMat, signal)
-
-    
-
-    
-    
-    
+    if twiddle_mat is None:
+        #Generate twiddle matrix
+        twiddle_mat = np.full((l_signal, l_signal), 0j)
+        for a in range(l_signal):
+            for b in range(l_signal):
+                twiddle_mat[a][b] += np.e**(-2j*(np.pi)*(a)*(b)/l_signal)
+                
+    #Return matrix multiplication
+    return np.matmul(twiddle_mat, signal)
--- fft.py
+++ fft.py
@@ -5,39 +5,45 @@ import math
 import numpy as np
 
 #Implementation of Cooley-Tukey fast fourier transform algorithm
-#[Inputs]   x: a 1D numpy array containing the signal to perform fft on
+#[Inputs]   signal: a 1D time-domain numpy array containing the signal to perform fft on
+#           twiddle_matrix: matrix of size len(signal) x len(signal) to sample twiddles from
 #[Outputs]  y: a 1D complex valued numpy array containing the dft of x
-def fft(x):
+def fft(signal, twiddle_mat=None):
     #Check dimensions
-    if len(x.shape) != 1 or len(x) < 1:
+    if len(signal.shape) != 1 or len(signal) < 1:
         raise Exception("Dimension error")
     
     #Extract factor from the length of x
-    factor1 = next_factor(len(x))
-    if factor1 == len(x):
+    factor1 = next_factor(len(signal))
+    if factor1 == len(signal):
         #The length of x is prime, proceed to do regular dft
-        return dft.dft(x)
+        return dft.dft(signal, twiddle_mat)
     
     #Second factor
-    factor2 = len(x) / factor1
+    factor2 = len(signal) / factor1
     
     #Rearrange into 2D matrix for divide-and-conquer
-    M = np.reshape(x, (factor1, factor2))
+    M = np.reshape(signal, (factor1, factor2))
     
     #Compute dft across columns
     for col in range(factor2):
-        M[:,col] = dft.dft(M[:,col])
+        M[:,col] = fft(M[:,col], twiddle_mat)
         
-    #Compute and apply twiddle matrix
-    T = np.array([[math.e**(-2j * math.pi / len(x) * a * b) 
-                for b in range(factor2)] 
-                for a in range(factor1)])
+    if twiddle_mat==None:
+        #Compute twiddle matrix
+        T = np.array([[math.e**(-2j * math.pi / len(signal) * a * b) 
+                    for b in range(factor2)] 
+                    for a in range(factor1)])
+    else:
+        #Draw twiddle from matrix
+        T = twiddle_mat[:factor1,:factor2]
+        
     #Apply inner product
     M = M * T
     
     #Compute dft across rows
     for row in range(factor1):
-        M[row,:] = dft.dft(M[row,:])
+        M[row,:] = fft(M[row,:], twiddle_mat)
     
     #Transpose and unravel
     y = np.reshape(np.transpose(M), (-1))
@@ -52,6 +58,9 @@ def next_factor(x):
         if i%x == 0:
             return i
     return x
+
+print(dft.dft(np.arange(15)))
+
 
 
 
